ac1f64e08dbc23b2a032317c87f40271723f6822
poukkyine-mdk <pyaephyokyaw.22121996@outlook.com>
2018-10-01 22:33:33 +1000

    start of the blackboard

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ var cassandra = require("cassandra-driver");
 var async = require('async');
 //connect to the cluster
 var client = new cassandra.Client({contactPoints:['127.0.0.1'],keyspace:'demo'});
-
-var express=require("express");
-var app=express();
-app.get("/",function(req,res){
-	//read users and print to console
+//read users and print to console
 client.execute("SELECT lastname, age, city, email, firstname FROM users WHERE email='bob@gg.com'", function(err,result){
 	if(!err){
 		if(result.rows.length>0){
@@ -21,6 +17,10 @@ client.execute("SELECT lastname, age, city, email, firstname FROM users WHERE em
 	}
 	
 })
+
+var express=require("express");
+var app=express();
+app.get("/",function(req,res){
 	res.send("Hello Angel");
 })
 app.listen(3000,function(){
